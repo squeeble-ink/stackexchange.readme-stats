781d609ecee556a928a50beb64338d6ae1c9fe8c
TessavWalstijn <twalstijn@outlook.com>
2023-04-24 16:47:00 +0200

    Added docker and index.js to run stand-alone

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import cors from 'cors'
+import stackExchange from './routes/stackExchange.route.js'
+
+const fallbackPort =
+  process.env.NODE_ENV === 'production' ? 3344 : 3040
+const port = process.env.PORT
+  ? parseInt(process.env.PORT, 10)
+  : fallbackPort
+
+const server = express()
+
+server.use(express.json())
+server.use(
+  express.urlencoded({
+    extended: true,
+    type: '*/x-www-form-urlencoded',
+  }),
+)
+
+server.use(cors())
+
+const api = express()
+
+api.use('/se', stackExchange)
+
+server.use('/', api)
+
+server.listen(port, (err) => {
+  if (err) throw err
+  console.log(`Listening on post ${port}`)
+})
---
+++ stack-readme-dev.dockerfile
@@ -0,0 +1,11 @@
+FROM node:18-alpine3.16
+WORKDIR /base
+ARG api_key
+COPY ./routes/ /base/routes/
+COPY ./index.js /base/
+COPY ./.gitignore /base/
+COPY ./.prettierrc /base/
+COPY ./package.json /base/
+ENV STACK_EXCHANGE_API_KEY $api_key
+RUN npm install
+CMD ["npm", "run", "dev"]
